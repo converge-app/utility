3a35d46ef368f8354849ac66c8307ae06f750691
kjuulh <hermansendev@gmail.com>
2019-10-01 22:41:32 +0200

    Add message

---
+++ Application.Utility/Models/Message.cs
@@ -0,0 +1,11 @@
+namespace Application.Utility.Models
+{
+    public class MessageObj
+    {
+        public MessageObj(string message)
+        {
+            Message = message;
+        }
+        public string Message { get; set; }
+    }
+}
