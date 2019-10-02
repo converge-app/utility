3292f15d8eab35be70a206399ec196771abb4bed
kjuulh <hermansendev@gmail.com>
2019-10-02 17:42:19 +0200

    Fixed settigns

--- Application.Utility/Startup/Settings.cs
+++ Application.Utility/Startup/Settings.cs
@@ -1,17 +1,17 @@
 using Application.Utility.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Application.Utility.Startup
 {
     public static class Settings
     {
-        public static AppSettings GetAppSettings(IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection GetAppSettings(this IServiceCollection services, IConfiguration configuration)
         {
             var appSettingsSection = configuration.GetSection("AppSettings");
             services.Configure<AppSettings>(appSettingsSection);
-            var appSettings = appSettingsSection.Get<AppSettings>();
-            return appSettings;
+            return services;
         }
     }
 }
